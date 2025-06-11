abbe94ac2a05ae9b96193742c5b9639ae8c71d96
Davidsonnj <davidsoncs45@gmail.com>
2025-06-10 22:08:54 -0300

    feat: main para teste

--- categorizer/src/main/java/edu/br/ifes/categorizer/App.java
+++ categorizer/src/main/java/edu/br/ifes/categorizer/App.java
@@ -1,17 +1,37 @@
 package edu.br.ifes.categorizer;
 import br.edu.ifes.mestrado.emailAPI.service.EmailSenderService;
+import edu.br.ifes.categorizer.Database.DAO.Implementations.EmailDAO;
 import edu.br.ifes.categorizer.EmailAPI.Controller.EmailController;
+import edu.br.ifes.categorizer.EmailAPI.Model.Email;
+import edu.br.ifes.categorizer.EmailAPI.Service.MarkEmail;
+import edu.br.ifes.categorizer.EmailAPI.View.EmailView;
+import edu.br.ifes.categorizer.GenAI.Perguntar;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
 public class App 
 {
     public static void main(String[] args) {
         EmailController emailController = new EmailController();
 
-        boolean enviado = emailController.sendEmail("davidsonifes@gmail.com", "Assunto Teste", "Corpo do e-mail");
-        System.out.println(enviado ? "✅ Enviado com sucesso" : "❌ Falha no envio");
+        List<Email> emails =emailController.emails(null,null,null);
+        for (Email email : emails){
+            EmailDAO emailDAO = new EmailDAO();
+            MarkEmail markEmail = new MarkEmail();
+            String status = Perguntar.getStatusEmail(email);
+
+            email.setStatus(status);
+
+            emailDAO.insert(email);
+            markEmail.wasRead(email.getUid());
+
+        }
+        /*
+        emailController.sendEmail("davidsonifes@gmail.com", "Teste", "teste");
+        EmailDAO emailDAO = new EmailDAO();
+        EmailView.displayEmails(emailDAO.findAll());
+
+         */
+
     }
 }
